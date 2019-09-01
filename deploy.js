0630b382c4b4f2ebb843078d3e9d32371120387c
tim crow <tim_crow@ymail.com>
2019-09-01 08:54:48 -0500

    adding .travis.yml and deploy.sh files

--- deploy.js
+++ deploy.js
@@ -22,8 +22,8 @@ function installPM2() {
 // transfers local project to the remote server
 function transferProjectToRemote(failed, successful) {
   return ssh.putDirectory(
-    '../hackathon-starter',
-    '/home/ubuntu/hackathon-starter-temp',
+    '../Riot-Express-Todo-List',
+    '/home/ubuntu/Riot-Express-Todo-List-temp',
     {
       recursive: true,
       concurrency: 1,
@@ -49,7 +49,7 @@ function transferProjectToRemote(failed, successful) {
 // creates a temporary folder on the remote server
 function createRemoteTempFolder() {
   return ssh.execCommand(
-    'rm -rf hackathon-starter-temp && mkdir hackathon-starter-temp', {
+    'rm -rf Riot-Express-Todo-List-temp && mkdirRiot-Express-Todo-List-temp', {
       cwd: '/home/ubuntu'
   });
 }
@@ -65,7 +65,7 @@ function stopRemoteServices() {
 // updates the project source on the server
 function updateRemoteApp() {
   return ssh.execCommand(
-    'cp -r hackathon-starter-temp/* hackathon-starter/ && rm -rf hackathon-starter-temp', {
+    'cp -r Riot-Express-Todo-List-temp/*Riot-Express-Todo-List/ && rm -rf Riot-Express-Todo-List-temp', {
       cwd: '/home/ubuntu'
   });
 }
@@ -73,7 +73,7 @@ function updateRemoteApp() {
 // restart mongodb and node services on the remote server
 function restartRemoteServices() {
   return ssh.execCommand(
-    'cd hackathon-starter && sudo service mongod start && pm2 start app.js', {
+    'cd Riot-Express-Todo-List && sudo service mongod start && pm2 start app.js', {
       cwd: '/home/ubuntu'
   });
 }
@@ -95,7 +95,7 @@ function sshConnect() {
       return installPM2();
     })
     .then(function() {
-      console.log('Creating `hackathon-starter-temp` folder.');
+      console.log('Creating `Riot-Express-Todo-List-temp` folder.');
       return createRemoteTempFolder();
     })
     .then(function(result) {
